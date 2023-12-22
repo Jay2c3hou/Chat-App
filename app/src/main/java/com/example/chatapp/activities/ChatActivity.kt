141c28dafcbd48fb9766361c9911caef5fe98fd1
yysc <2621007884@qq.com>
2023-12-22 21:23:40 +0800

    完成聊天页面布局

---
+++ app/src/main/java/com/example/chatapp/activities/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.example.chatapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chatapp.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
